ffcf479225acb6a9b33b50c20de077c2234c50ae
Jordan Brewer <brewdamaster@gmail.com>
2016-08-10 17:54:01 -0400

    adding files

--- acceptable_use/pw_sessions_policy/pw_sessions_policy.sh
+++ acceptable_use/pw_sessions_policy/pw_sessions_policy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-user=$(id -un)
-pwpolicy -u $user -setpolicy "minChar=7 usingHistory=4 requiresAlpha=1 requiresNumeric=1 maxMinutesUntilChangePassword=2" # password policy: change every 90 (129600) testing with 5, 7 char or more, remembers and rejects last 4 passwords, 1 alpha and 1 numeric minimums
+pwpolicy -setpolicy "minChar=7 usingHistory=4 requiresAlpha=1 requiresNumeric=1 canModifyPasswordforSelf=1 newPasswordRequired=0 maxMinutesUntilChangePassword=2" # password policy: change every 90 (129600) testing with 5, 7 char or more, remembers and rejects last 4 passwords, 1 alpha and 1 numeric minimums
 exit 0
+
+
+#-getglobalpolicy before -setglobalpolicy maxMinutesUntilChangePassword is being a pain
