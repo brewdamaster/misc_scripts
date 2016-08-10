1638483a6108637b48ed25d31408673d47ddce0b
Jordan Brewer <brewdamaster@gmail.com>
2016-08-10 12:17:07 -0400

    updated .sh

--- acceptable_use/pw_sessions_policy/pw_sessions_policy.sh
+++ acceptable_use/pw_sessions_policy/pw_sessions_policy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-test
+user=$(id -un)
+pwpolicy -u $user -setpolicy "minChar=7 usingHistory=4 requiresAlpha=1 requiresNumeric=1 maxMinutesUntilChangePassword=15" # password policy: change every 90 (129600) testing with 15, 7 char or more, remembers and rejects last 4 passwords, 1 alpha and 1 numeric minimums
 exit 0
