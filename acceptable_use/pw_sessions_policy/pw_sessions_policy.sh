8e3d501e061b28455da73795cb4c7bcac3443353
Jordan <brewdamaster@gmail.com>
2016-08-11 09:50:14 -0400

    Delete pw_sessions_policy.sh

--- acceptable_use/pw_sessions_policy/pw_sessions_policy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pwpolicy -setpolicy "minChar=7 usingHistory=4 requiresAlpha=1 requiresNumeric=1 canModifyPasswordforSelf=1 newPasswordRequired=0 maxMinutesUntilChangePassword=2" # password policy: change every 90 (129600) testing with 5, 7 char or more, remembers and rejects last 4 passwords, 1 alpha and 1 numeric minimums
-exit 0
-
-
-#-getglobalpolicy before -setglobalpolicy maxMinutesUntilChangePassword is being a pain
