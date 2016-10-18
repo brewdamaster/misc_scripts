7e3c71f2f1176600215eaa758aa9b5de0d6956ea
Jordan Brewer <brewdamaster@gmail.com>
2016-10-18 15:39:34 -0400

    small revision

--- acceptable_use/pw_sessions_policy/pw_policy_olduser.sh
+++ acceptable_use/pw_sessions_policy/pw_policy_olduser.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
-pwpolicy -clearaccountpolicies
-pwpolicy -setglobalpolicy "minChars=8 requiresNumeric=1"
+#!/bin/sh
+user=$(id -F)
+sudo pwpolicy -clearaccountpolicies
+sudo pwpolicy -setglobalpolicy "minChars=8 requiresNumeric=1"
+sudo pwpolicy -u $user -setpolicy "newPasswordRequired=1"
